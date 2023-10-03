0220999b76557e98fe0a26353ae845cbb51bd3b9
pawelce <szkolacecot@gmail.com>
2023-10-03 10:16:53 +0200

    Praca na lekcji/

--- 03.10.2023/03.10.2023/03.10.2023.cpp
+++ 03.10.2023/03.10.2023/03.10.2023.cpp
@@ -1,20 +1,64 @@
 ﻿#include <iostream>
-int zmienna = 1;
-switch (zmienna) {
-case 1:
-    //jakiś kod nr 1
-//np wywołanie funkcji, klasy, itd...
-    break;
-case 2:
-    //jakiś kod nr 2
-//np wywołanie funkcji, klasy, itd...
-    break;
-case 3:
-    //jakiś kod nr 3
-//np wywołanie funkcji, klasy, itd...
-    break;
-default:
-    //tutaj wykona się zawsze kod domyślnie	
-//np wywołanie funkcji, klasy, itd...
-    break;
+using namespace std;
+float dodawanie(float liczba1, float liczba2);
+float odejmowanie(float liczba1, float liczba2);
+float mnożenie(float liczba1, float liczba2);
+void dzielenie(float liczba1, float liczba2);
+int main()
+{
+    setlocale(LC_ALL, "");
+    float liczba1, liczba2;
+    int menu;
+    cout << "Kalkulator z switch case\n";
+    cout << "dodawanie 1, odejmowanie 2, mnożenie 3,dzielenie 4" << endl;
+    cout << "Wybierz operacje = "; cin >> menu;
+    cout << "Podaj liczbe1 ="; cin >> liczba1;
+    cout << "Podaj liczbe1 ="; cin >> liczba2;
+
+    switch (menu)
+    {
+        case 1:
+        cout << "Dodawanie = " << dodawanie(liczba1, liczba2) << endl;
+        break;
+        case 2:
+        cout << "Odejmowanie= " << odejmowanie(liczba1, liczba2) << endl;
+        break;
+            case 3:
+                cout << "Mnożenie = " << mnożenie(liczba1, liczba2) << endl;
+                break;
+            case 4:
+                dzielenie(liczba1, liczba2);
+        break;
+            default:
+                cout << "Nie zdefiniowano operacji:rozpocznij od początku" << endl;
+                break;
+    }
+
+    system("pause");
+    return 0;
+}
+
+float dodawanie(float liczba1, float liczba2)
+{
+    return liczba1 + liczba2;
+}
+
+float odejmowanie(float liczba1, float liczba2)
+{
+    return liczba1 - liczba2;
+}
+
+float mnożenie(float liczba1, float liczba2)
+{
+    return liczba1 * liczba2;
+}
+
+void dzielenie(float liczba1, float liczba2)
+{
+    if (liczba1 != 0) {
+        cout << "dzielenie = " << liczba1 / liczba2 << endl;
+    }
+    else {
+        cout << "nie dziel szlachcicu przez zero " << endl;
+    }
 }
---
+++ Praca na lekcji/Praca na lekcji/Praca na lekcji.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+double potegowanie(float a);
+double pierwiastkowanie(float a);
+float obli1(float a, float b);
+float obli2(float a, float b);
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    float b,a;
+    int menu;
+    cout << "Kalkulator z switch case\n";
+    cout << "potęgowanie 1,pierwiastek z (a) 2, a+b/a*b 3,(a*b-b*a)/a 4" << endl;
+    cout << "Wybierz operacje = "; cin >> menu;
+    cout << "Podaj a ="; cin >> a;
+    cout << "Podaj b ="; cin >> b;
+    
+
+    switch (menu)
+    {
+    case 1:
+        cout << "potęgowanie = " << potegowanie(a) << endl;
+        break;
+    case 2:
+        cout << "pierwiastek z (a)= " << pierwiastkowanie(a) << endl;
+        break;
+    case 3:
+        cout << "a+b/a*b = " << obli1(a, b) << endl;
+        break;
+    case 4:
+        obli2(a, b);
+        break;
+    default:
+        cout << "Nie zdefiniowano operacji:rozpocznij od początku" << endl;
+        break;
+    }
+
+    system("pause");
+    return 0;
+}
+
+float obli1(float a, float b)
+{
+    return a + b - a * b;
+}
+
+
+
+double potegowanie(float a)
+{
+     return pow( a, 2);
+}
+
+double pierwiastkowanie(float a)
+{
+    return sqrt(a);
+}
+
+float obli2(float a, float b)
+{
+    return (a * b - b * a) / a;
+}
